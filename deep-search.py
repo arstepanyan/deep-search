2f40a493f65d2c1b277f13b0bc70c9061fe8f9d8
Araks Stepanyan <arax.stepanyan@gmail.com>
2019-07-10 23:18:20 -0700

    remove arguments related to searching by input image

--- deep-search.py
+++ deep-search.py
@@ -25,12 +25,9 @@ def main():
                                      usage='The following options are acceptable:\n'
                                            '* Index the catalog:  '
                                            'python %(prog)s index --catalog_path path-to-catalog\n'
-                                           '* Search text in the catalog:  '
+                                           '* Search in the catalog:  '
                                            'python %(prog)s search --catalog_path path-to-catalog '
-                                           '--results_path path-to-results --text text1 text2 textN\n'
-                                           '* Search image in the catalog:  '
-                                           'python %(prog)s search --catalog_path path-to-catalog '
-                                           '--results_path path-to-results --image path-to-image',
+                                           '--results_path path-to-results --text text1 text2 textN\n',
                                      description='Search text or image in your catalog.')
 
     parser.add_argument('mode', help='Need to provide an action: search or index.', choices=['search', 'index'])
@@ -38,20 +35,15 @@ def main():
     args, leftovers = parser.parse_known_args()
 
     catalog = Catalog(args.catalog_path)
-    print(catalog.indexed)
 
     if args.mode == 'index':
         catalog.index()
     else:
         parser.add_argument('--results_path', help='Path to the catalog directory.', type=dir_path, required=True)
-        command_group = parser.add_mutually_exclusive_group(required=True)
-        command_group.add_argument('--text', nargs='+')
-        command_group.add_argument('--image', help='Path to an image.', type=file_path)
+        parser.add_argument('--text', nargs='+', required=True)
         args = vars(parser.parse_args())
-        if args['text']:
-            catalog.search_text(args['text'])
-        else:
-            catalog.search_image(args['image'])
+        catalog.search_text(args['text'])
+
         print(args['text'])
 
 
